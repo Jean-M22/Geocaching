50bc6c856672a069686ce227ce78c8271fbd1d21
JeanM22 <mera.jean.robert@gmail.com>
2022-11-24 15:01:13 +0200

    set status to 200 before serving file

--- Fun with Files/index.js
+++ Fun with Files/index.js
@@ -4,5 +4,5 @@ const app = require("..");
 app.get("/FunWithFiles/Puzzle.rar", (req, res) => {
     const filePath = path.join(__dirname, "public", "Puzzle.rar");
     console.log("Serving file " + filePath);
-    return res.sendFile(filePath);
+    return res.status(200).sendFile(filePath);
 });
