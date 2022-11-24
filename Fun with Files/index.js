81ba41d5ff4163d7159e2a0e9fae456b8d8a5364
JeanM22 <mera.jean.robert@gmail.com>
2022-11-24 11:49:31 +0200

    Add stuff

--- Fun with Files/index.js
+++ Fun with Files/index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const app = require("..");
 
-app.get("/GC9VF7F/public/Puzzle.rar", (req, res) => {
+app.get("/FunWithFiles/Puzzle.rar", (req, res) => {
     const filePath = path.join(__dirname, "public", "Puzzle.rar");
     console.log("Serving file " + filePath);
     return res.sendFile(filePath);
