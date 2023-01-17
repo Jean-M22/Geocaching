04b326ea179d6b13e859c5840a6da49724d4ce35
JeanM22 <mera.jean.robert@gmail.com>
2023-01-17 18:38:56 +0200

    Add form

--- _external/@Dino_Master2000/Approximations/index.load.ts
+++ _external/@Dino_Master2000/Approximations/index.load.ts
@@ -4,3 +4,7 @@ import { app } from "../../..";
 app.get("/_external/Dino_Master2000/Approximations/geochecker", (req, res) => {
     res.sendFile(join(__dirname, "/public/geochecker.html"));
 });
+
+app.post("/_external/Dino_Master2000/Approximations/result", (req, res) => {
+    res.send(req.body.coordinates);
+})
