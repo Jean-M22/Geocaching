361569e0459b4f17943c03e528f2ef5f35e2eef7
JeanM22 <mera.jean.robert@gmail.com>
2023-01-17 20:08:17 +0200

    Update folders

--- _external/Dino_Master2000/LPSG Ark Survival Evolved/index.load.ts
+++ _external/Dino_Master2000/LPSG Ark Survival Evolved/index.load.ts
@@ -6,7 +6,11 @@ app.get("/_external/Dino_Master2000/LPSGArkSurvivalEvolved/puzzle", (req, res) =
     res.sendFile(join(__dirname, "/public/puzzle.html"));
 });
 
+app.get("/_external/Dino_Master2000/LPSGArkSurvivalEvolved/result", (req, res) => {
+    res.redirect(301, "/_external/Dino_Master2000/LPSGArkSurvivalEvolved/puzzle");
+})
+
 app.post("/_external/Dino_Master2000/LPSGArkSurvivalEvolved/result", (req, res) => {
-    if (req.body.password != password) return res.redirect(301, "/_external/Dino_Master2000/LPSGArkSurvivalEvolved/puzzle?incorrect");
+    if (req.body.password.toLowerCase() != password) return res.redirect(301, "/_external/Dino_Master2000/LPSGArkSurvivalEvolved/puzzle?incorrect");
     res.sendFile(join(__dirname, "/data/result.jpg"));
 });
